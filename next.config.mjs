2f55e91245b2758b4bb98bb48fae131d0d275fe4
alireza01 <alireza01@example.com>
2025-06-11 20:43:08 +0330

    Update dependencies and configuration files

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,6 @@ const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
   },
-  experimental: {
-    serverActions: true,
-  },
 };
 
 export default nextConfig;
